504e6e09d422c39bb7a64ce7b6db556eb273a205
Antonasdf <antnnn@live.se>
2021-04-22 14:29:03 +0200

    Nya klasser

---
+++ src/User.java
@@ -0,0 +1,32 @@
+public class User {
+    private String firstName;
+    private String lastName;
+    private int personalNumber;
+    //private int countError;
+    //private int countBooks;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPersonalNumber() {
+        return personalNumber;
+    }
+
+    public void setPersonalNumber(int personalNumber) {
+        this.personalNumber = personalNumber;
+    }
+
+}
--- src/testklass1.java
+++ src/testklass1.java
@@ -1,14 +1,6 @@
 public class testklass1 {
     public static void main(String[] args) {
-        System.out.println("Sören");
-        System.out.println("Samma elelr");
-        System.out.println("hejsan max");
 
-        System.out.println("håkans dotter!!!!");
-        System.out.println("Kukas xddddddd");
-        System.out.println("ahahahjhjhjhah");
-        System.out.println("lukkeeeee");
-        System.out.println("efterblivet");
     }
 }
 
