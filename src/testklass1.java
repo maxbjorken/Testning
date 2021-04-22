ff29618439d8a2472a573754a4f8cb29d23788c6
Antonasdf <antnnn@live.se>
2021-04-22 12:22:35 +0200

    Test

--- src/testklass1.java
+++ src/testklass1.java
@@ -2,5 +2,6 @@ public class testklass1 {
     public static void main(String[] args) {
         System.out.println("Sören");
         System.out.println("Samma elelr");
+        System.out.println("hejsan max");
     }
 }
