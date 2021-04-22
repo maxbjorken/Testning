eefed410f97fe29a7c22985b43cd1f0935d949ef
maxbjorken <78048713+maxbjorken@users.noreply.github.com>
2021-04-22 14:30:21 +0200

    Första commiten! Grattis

--- src/testklass1.java
+++ src/testklass1.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
+
 public class testklass1 {
     public static void main(String[] args) {
-        System.out.println("Sören");
-        System.out.println("Samma elelr");
-        System.out.println("hejsan max");
 
-        System.out.println("håkans dotter!!!!");
-        System.out.println("Kukas xddddddd");
-        System.out.println("ahahahjhjhjhah");
-        System.out.println("lukkeeeee");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("***********************");
+        System.out.println("Välkommen till Hultsfred Stadsbibliotek");
+        System.out.println("************************");
+
+        System.out.println("Välj ditt val (1 eller 2)");
+        System.out.println("1. Låna/Lämna tillbaka bok");
+        System.out.println("2. Registrera dig!");
+        System.out.println();
+       int val = scan.nextInt();
+
+        switch (val) {
+
+            case 1:
+            System.out.println("Lämna tillbaka boken eller låna?");
+            break;
+
+            case 2:
+                System.out.println("Registrera");
+                break;
+        }
+
+
+
     }
 }
 
